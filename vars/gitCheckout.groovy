e5ceac68124203098b54b645d41d2ce696724dd9
Sumit Jeswani <66941930+jeswanisumit1999@users.noreply.github.com>
2024-01-06 02:41:29 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,7 @@
-def call(Map stageParams){
-    checkout{
-        scmGit(
-            branches: [[name: stageParams.branch]], 
-            extensions: [], 
-            userRemoteConfigs: [[url: stageParams.repoUrl]]
-        )
-    }
-}
+def call(Map stageParams) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.repoUrl ]]
+    ])
+  }
